0470e5dc8115056127deae60c9a9d5b74414d9da
p-duke <peter.duke@bouncex.com>
2021-07-08 11:57:17 -0400

    Add CSP to response headers

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,13 @@ function getUnauthorizedResponse(req) {
 
 /* Routes */
 router.get('/', (req, res) => {
-  var csp = "default-src 'self' \
+  var csp = "script-src tag.wknd.ai assets.bounceexchange.com api.bounceexchange.com stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js https://code.jquery.com/jquery-3.5.1.min.js https://smart-tag.bounceexchange.com/dist/ijs_all_modules_dev.js dev.bounceexchange.com 'unsafe-inline' 'unsafe-eval'; \
+  style-src assets.bounceexchange.com stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css 'unsafe-inline'; \
+  img-src 'self' assets.bounceexchange.com events.bouncex.net dev.bounceexchange.com ssp.behave.com *.bidswitch.net ums.acuityplatform.com beacon.lynx.cognitivlabs.com odr.mookie1.com 'strict-dynamic'; \
+  font-src assets.bounceexchange.com; \
+  form-action api.bounceexchange.com; \
+  connect-src events.bouncex.net https://coupons.bounceexchange.com https://*.cdnwidget.com https://*.cdnbasket.net dev.bounceexchange.com; \
+  frame-src 'self'; \
   ";
   res.set("Content-Security-Policy", csp);
   res.render('index');
