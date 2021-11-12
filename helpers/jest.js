9318ceefd94322ae92a299f229e40c88ef654eb1
Peter Duke <peter.duke@wunderkind.co>
2021-11-12 13:23:59 -0500

    window location mock

--- helpers/jest.js
+++ helpers/jest.js
@@ -82,7 +82,7 @@ let img = new Image();
 window.document.createElement = jest.fn(() => img);
 // Then you can test img.src after code has run
 
-// Mock jQuery.on / off
+// Mock jQuery.on / off - Remove all events if you dont have original function
 const removeAllEvents = () => {
   var old = document.querySelector('body');
   var newNode = old.cloneNode(true);
@@ -96,4 +96,50 @@ beforeEach(() => {
 bouncex.on = jest.fn((_, __, callback) => {
   document.querySelector('input').addEventListener('blur', callback);
 });
+
+// Mock window.location.href
+const defineProperty = (obj, property, state) => {
+  Object.defineProperty(obj, property, {
+    value: state
+  });
+};
+
+beforeEach(() => {
+  defineProperty(window, 'location', { href: 'www.ronloveslilsebastian.com' });
+});
+
+afterEach(() => {
+  window.location.href = 'www.ronloveslilsebastian.com';
+});
+
+// Mock document.cookie
+beforeEach(() => {
+  Object.defineProperty(document, 'cookie', {
+    writable: true,
+    value: 'status=active',
+  });
+});
+
+// Mock a new Function or Class entirely
+
+// import Criteria from './criteria';
+//
+jest.mock('./criteria', () => {
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation(() => {
+      return {
+        evaluate: jest.fn().mockReturnValue(true),
+      };
+    }),
+  }
+});
+
+// To change the return value
+Criteria.mockImplementation(() => {
+  return {
+    evaluate: jest.fn().mockReturnValue(false)
+  };
+});
+
 
